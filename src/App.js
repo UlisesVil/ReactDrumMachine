5aadc9823c4f4aada7c4e0a6f18f9f9d64eab8ab
github_UlisesVil <ulisesvil5@hotmail.com>
2020-07-12 00:30:11 -0500

    Package.json modiefied to published project on github pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Project React Drum Machine in Progress
         </p>
         <a
           className="App-link"
