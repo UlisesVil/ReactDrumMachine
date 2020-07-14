fbc1d798204dec6b7e8416ad7b4213afb88e3e9d
github_ulisesvil <ulisesvil5@hotmail.com>
2020-07-14 01:51:38 -0500

    Funtionality, visuals, test 8/8 finished

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import DrumMachine from './modules/drumMachine';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <DrumMachine />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
